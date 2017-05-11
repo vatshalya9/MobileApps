19a01f8c2777d3a3aa02d314816a198d8324454b
vatshalya9 <vatshalya.d009@gmail.com>
2017-05-10 22:21:48 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿'use strict';
+
+// Declare app level module which depends on views, and components
+var app = angular.module('myApp', [
+]);
+
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    //$locationProvider.hashPrefix('!');
+
+    //$routeProvider.otherwise({ redirectTo: '/view1' });
+
+    $routeProvider.when('/', {
+        templateUrl: 'ng-app/view/home.html',
+        controller: 'homeController',
+        caseInsensitiveMatch: true,
+    });
+}]);
